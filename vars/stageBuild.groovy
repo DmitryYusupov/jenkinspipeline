3a907483957c9d2ff36f56aa15d860767b69af54
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-01-27 14:53:02 +0300

    Try to do jenkins pipeline

--- vars/stageBuild.groovy
+++ vars/stageBuild.groovy
@@ -2,5 +2,11 @@ import pipeline.stages.build.config.BuildStageConfig
 
 def doBuild(BuildStageConfig stageConfig) {
     stage(stageConfig.stageName) {
+        def gradleCommand = stageConfig.gradleCommand
+        if (gradleCommand != null && gradleCommand.isEmpty()) {
+            buildToolsGradle.call(gradleCommand)
+        }
+
+
     }
 }
