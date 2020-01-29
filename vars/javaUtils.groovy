d098860ab138b3042d9adef71d582bc2d0c9316c
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-01-29 18:10:23 +0300

    Added parsing of xml

--- vars/javaUtils.groovy
+++ vars/javaUtils.groovy
@@ -1,6 +1,6 @@
 
 def setJavaHome(String jenkinsJavaName){
-    env.JAVA_HOME="${tool "'" + jenkinsJavaName "'"+ ""}"
+    env.JAVA_HOME="${tool "" + jenkinsJavaName + ""}"
     env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
 
 }
