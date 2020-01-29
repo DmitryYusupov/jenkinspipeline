05260fc87787f4eb4366f09d48ef636c750f0e08
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-01-29 17:17:27 +0300

    Added parsing of xml

--- vars/stageCheckout.groovy
+++ vars/stageCheckout.groovy
@@ -5,7 +5,7 @@ def doCheckout(CheckoutStageConfig checkoutConfig) {
     println("============================BEGIN $checkoutConfig.label ============================")
     stage(checkoutConfig.label) {
         checkout scm: [$class           : 'GitSCM',
-                       branches         : [[name: checkoutConfig.gitConfig.brunch]],
+                       branches         : [[name: checkoutConfig.gitConfig.branch]],
                        userRemoteConfigs: [[url: checkoutConfig.gitConfig.url]]
         ]
     }
