0ca01978fc5f0401b5c7a7647d048abc5c174249
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-04 17:00:30 +0300

    Added os checking

--- vars/javaUtils.groovy
+++ vars/javaUtils.groovy
@@ -1,5 +1,14 @@
+import utils.os.Os
+import utils.os.OsUtils
 
-def setJavaHome(String jenkinsJavaName){
-    env.JAVA_HOME="${tool "" + jenkinsJavaName + ""}"
-    env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
+def setJavaHome(String jenkinsJavaName) {
+    def os = OsUtils.getOS()
+
+    env.JAVA_HOME = "${tool "" + jenkinsJavaName + ""}"
+    if (Os.WINDOWS.equals(os)) {
+        env.PATH="${env.JAVA_HOME}/bin%${env.PATH}%"
+    } else {
+        //linux
+        env.PATH = "${env.JAVA_HOME}/bin:${env.PATH}"
+    }
 }
