e1bfa13755457b6fe9cec4f941d007edf9b1cb5c
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-13 16:17:20 +0300

    Added ability to login to docker registry

--- vars/stageDockerise.groovy
+++ vars/stageDockerise.groovy
@@ -244,6 +244,17 @@ private void deleteImageIfNeed(List<DockerImage> images, int threshold) {
 
 private boolean loginToRegistry(AccessConfig accessConfig) {
     println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+    withCredentials([usernamePassword(credentialsId: 'DOCKER_USER_DEV', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        // available as an env variable, but will be masked if you try to print it out any which way
+        // note: single quotes prevent Groovy interpolation; expansion is by Bourne Shell, which is what you want
+        sh 'echo $PASSWORD'
+        // also available as a Groovy variable
+        echo USERNAME
+        // or inside double quotes for string interpolation
+        echo "username is $USERNAME"
+    }
+
     println(accessConfig.login)
     println(accessConfig.dokcerRegistryUrl)
     println(env.BUILD_ID)
