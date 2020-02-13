366f35b81a6ae1f82e829538a201404ebc8c57ac
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-13 15:10:45 +0300

    Added comments for docker step

--- vars/stageDockerise.groovy
+++ vars/stageDockerise.groovy
@@ -10,7 +10,13 @@ import utils.os.OsUtils
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-def doContainerisation(DockeriseStageConfig dockeriseConfig, PipelineContext pipelineContext) {
+/**
+ * Execute dockerisation: create, push images
+ *
+ * @param dockeriseConfig Config with data
+ * @param pipelineContext Context which stores info about pipeline progress
+ */
+void doContainerisation(DockeriseStageConfig dockeriseConfig, PipelineContext pipelineContext) {
     println("============================BEGIN $dockeriseConfig.label ============================")
     stage(dockeriseConfig.label) {
         def image = buildImage(dockeriseConfig.buildImageConfig, pipelineContext)
@@ -28,6 +34,13 @@ def doContainerisation(DockeriseStageConfig dockeriseConfig, PipelineContext pip
     println("============================END $dockeriseConfig.label ============================")
 }
 
+/**
+ * Create image
+ *
+ * @param buildImageConfig Contains information about image
+ * @param pipelineContext Context which stores info about pipeline progress
+ * @return image. Has type org.jenkinsci.plugins.docker.workflow.Docker$Image
+ */
 private def buildImage(BuildImageConfig buildImageConfig, PipelineContext pipelineContext) {
 
     def curStageContext = new DockeriseStageContext()
@@ -51,15 +64,26 @@ private def buildImage(BuildImageConfig buildImageConfig, PipelineContext pipeli
     }
 }
 
-def revertStageChanges(Exception exception, PipelineContext pipelineContext) {
+/**
+ * Execute revert action for dockerise stage. For example, if error occurred we delete image which was build on this step
+ *
+ * @param exception
+ * @param pipelineContext Context which stores info about pipeline progress
+ */
+void revertStageChanges(Exception exception, PipelineContext pipelineContext) {
     if (pipelineContext.dockeriseStageContext != null) {
         if (pipelineContext.dockeriseStageContext.buildStageContext != null) {
             revertBuildImageStageChanges(exception, pipelineContext.dockeriseStageContext.buildStageContext)
         }
     }
 }
-
-private def revertBuildImageStageChanges(Exception exception, BuildImageStageContext buildImageStageContext) {
+/**
+ * Delete image if pipeline has finished with error
+ *
+ * @param exception
+ * @param buildImageStageContext Context which stores information about docker stage
+ */
+private void revertBuildImageStageChanges(Exception exception, BuildImageStageContext buildImageStageContext) {
     println("----BEGIN ErrorHandling <DockeriseStage> ----")
     try {
         def imageToDelete = buildImageStageContext.image + ":" + buildImageStageContext.tag
@@ -71,7 +95,19 @@ private def revertBuildImageStageChanges(Exception exception, BuildImageStageCon
     println("----END ErrorHandling <DockeriseStage> ----")
 }
 
-private deleteImagesIfNumberOfStoredImagesHasExpired(int maxImagesToStore, String imageName, String imageTag, String imageTagPrefix) {
+/**
+ * Delete images if current number of stored images is greater than threshold value
+ *
+ * See on possible image info:
+ * Full name: my-image:env_dev_1 (name: my-image, tag: env_dev_1, tag_prefix: env_dev_)
+ *
+ * @param maxImagesToStore threshold value, determines how many images we store
+ * @param imageName Image name
+ * @param imageTag Image tag
+ * @param imageTagPrefix tag prefix
+ *
+ */
+private void deleteImagesIfNumberOfStoredImagesHasExpired(int maxImagesToStore, String imageName, String imageTag, String imageTagPrefix) {
     println("-----------BEGIN. Dockerise. Clean old images-----------------")
     def command = getCommandToGetDockerImages(imageName, imageTag, imageTagPrefix)
     def output = osUtils.runCommandReturningOutput(command)
@@ -83,6 +119,18 @@ private deleteImagesIfNumberOfStoredImagesHasExpired(int maxImagesToStore, Strin
     println("-----------END. Dockerise. Clean old images-----------------")
 }
 
+/**
+ * Return command which will return images with defined image name and tag
+ *
+ * See on possible image info:
+ * Full name: my-image:env_dev_1 (name: my-image, tag: env_dev_1, tag_prefix: env_dev_)
+ *
+ * @param imageName Image name
+ * @param imageTag Image tag
+ * @param imageTagPrefix Image tag prefix
+ *
+ * @return command which will return images with defined image name and tag
+ */
 private String getCommandToGetDockerImages(String imageName, String imageTag, String imageTagPrefix) {
     switch (OsUtils.getOS()) {
         case Os.WINDOWS:
@@ -124,7 +172,17 @@ class DockerImage {
 
     }
 }
-
+/**
+ * We have output which contains data about docker image, for example:
+ * 'My-image            env1_79                    5bc41458c38d        30 minutes ago      291MB'
+ * 'other-image         env1_80                    5bc41458c3wd        32 minutes ago      291MB'
+ *
+ * Method parses output and convert it to objects which include imageName, imageTag and imageId
+ *
+ * @param outputStr output of docker images command
+ * @param imageName. We want to return the only image with several tags.
+ * @return objects which include imageName, imageTag and imageId
+ */
 private List<DockerImage> parseDockerImagesDataFromOutputString(String outputStr, String imageName) {
     List<DockerImage> result = new ArrayList<>()
 
@@ -153,6 +211,14 @@ private List<DockerImage> parseDockerImagesDataFromOutputString(String outputStr
     return result
 }
 
+/**
+ * Delete images if current number of images is greater or equals to threshold.
+ * We want to store <threshold> images with defined image name and tag prefix
+ *
+ * @param images Images with predefined imagename and tag prefix, ordered by creationDate,
+ * so the oldest are in the end.
+ * @param threshold Desired number of images to store
+ */
 private void deleteImageIfNeed(List<DockerImage> images, int threshold) {
     if (images.size() >= threshold) {
         println("-----------BEGIN. Dockerise. Exec delete old images-----------------")
@@ -164,7 +230,7 @@ private void deleteImageIfNeed(List<DockerImage> images, int threshold) {
             def isSuccessfullyDeleted = osUtils.runCommandReturningStatusAsBool("docker rmi $image.id")
             if (isSuccessfullyDeleted) {
                 println("Image '$imageInfo' was successfully deleted")
-            }else{
+            } else {
                 println("WARNING WARNING Image '$imageInfo' WAS NOT DELETED!")
             }
         }
