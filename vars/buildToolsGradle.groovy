8737c45a81b413556226647dd5e8f3156f903a25
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-13 12:54:39 +0300

    Fixed osUtils

--- vars/buildToolsGradle.groovy
+++ vars/buildToolsGradle.groovy
@@ -1,12 +1,12 @@
 import pipeline.stages.common.commands.GradleCommand
 
-static void call(GradleCommand gradleCommand) {
+void call(GradleCommand gradleCommand) {
     println("DD")
     println(gradleCommand.command + gradleCommand.utility)
     osUtils.runCommand(gradleCommand.command + gradleCommand.utility)
 }
 
-static void call(Collection<GradleCommand> commands) {
+void call(Collection<GradleCommand> commands) {
     for (GradleCommand command : commands) {
         call(command)
     }
