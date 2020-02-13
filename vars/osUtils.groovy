9a73a8231fad8788fb3cb7e714c2435a55ba8dfe
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-13 12:46:05 +0300

    Added extra methods to OsUtils

--- vars/osUtils.groovy
+++ vars/osUtils.groovy
@@ -1,13 +1,39 @@
 import utils.os.Os
 import utils.os.OsUtils
 
-def runCommandReturningOutput(String command) {
+String runCommandReturningOutput(String command) {
     switch (OsUtils.getOS()) {
         case Os.WINDOWS:
             return bat(returnStdout: true, script: command).trim()
 
         case Os.UNIX:
             return sh(returnStdout: true, script: command).trim()
+
+        default:
+            throw new RuntimeException("No utility detected to execute gradle command '$deleteCreateImageCommand'")
+    }
+}
+
+int runCommandReturningStatus(String command) {
+    switch (OsUtils.getOS()) {
+        case Os.WINDOWS:
+            return bat(returnStatus: true, script: command)
+
+        case Os.UNIX:
+            return sh(returnStatus: true, script: command)
+
+        default:
+            throw new RuntimeException("No utility detected to execute gradle command '$deleteCreateImageCommand'")
+    }
+}
+
+boolean runCommandReturningStatusAsBool(String command) {
+    switch (OsUtils.getOS()) {
+        case Os.WINDOWS:
+            return bat(returnStatus: true, script: command) == 0
+
+        case Os.UNIX:
+            return sh(returnStatus: true, script: command) == 0
 
         default:
             throw new RuntimeException("No utility detected to execute gradle command '$deleteCreateImageCommand'")
