81e7128492a2933587cb48eeba3a2474b91fdc59
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-02-27 15:57:19 +0300

    Added ability to revert stage actions for dockerise stage

--- vars/exceptionHandler.groovy
+++ vars/exceptionHandler.groovy
@@ -1,8 +1,11 @@
 import pipeline.stages.common.stage.PipelineContext
 import pipeline.stages.dockerise.exception.DockeriseStageException
 
-def handleException(Exception e, PipelineContext context) {
+def handleException(PipelineContext context) {
+
+    Exception e = context.exception
     println("Pipeline Exception " + e.toString())
+    e.printStackTrace();
 
     if (e instanceof DockeriseStageException) {
         stageDockerise.handleException(context)
